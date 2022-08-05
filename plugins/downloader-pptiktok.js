ed4e8abe3a5aae1e9740eaa997f02cfa45c2e737
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-05 09:30:04 +0700

    Update downloader-pptiktok.js

--- plugins/downloader-pptiktok.js
+++ plugins/downloader-pptiktok.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, text }) => {
 if (!text) throw '*[❗INFO❗] Masukan Nama User Tiktok Yang Ingin Diambil Fotonya*'
-let res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=9b817532fadff8fc7cb86862`
+let res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=azzbot`
 conn.sendFile(m.chat, res, 'error.jpg', `*[ ✔ ] Sukses Mengambil Foto User ${text}*`, m, false)
 }
 handler.help = ['tiktokfoto <user>']
